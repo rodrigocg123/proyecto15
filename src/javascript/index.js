7af3552f9b2b4ebc6d415fa0c195e933cfb22865
curso11 <rocasgut@gmail.com>
2024-12-04 13:58:08 +0100

    boton alerta

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,4 @@
-console.log('Hola mundo... por segunda vez');
+document.getElementById('boton').onclick = function() {
+    alert('Me ha presionado... me voy de vacacion a relajarme');
+}
+console.log('Hola mundo... por tercera vez');
