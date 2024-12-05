6e9dc4e73f9757036e7f3094a9acf91ed4166293
curso11 <rocasgut@gmail.com>
2024-12-05 13:53:39 +0100

    listas

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,37 @@
-document.getElementById('boton').onclick = function() {
-    alert('Me ha presionado... me voy de vacacion a relajarme');
+$(document).ready( function() {
+    solicitudes  = ["Mensaje1", "Hola", "Adiós"];
+
+    for(i = 0 ; i < solicitudes.length   ; i++) {
+        $("#maestro").append(
+            $("<li>").text(solicitudes[i])
+        )
+    }
+
+
+});
+let solicitudes = [solicitud,solicitud,solicitud];
+
+for(i = 0; i < solicitud.length; i++) {
+    $("#maestro").append(
+        $("<li>").text(solicitudes[i].apellido)
+    )
 }
-console.log('Hola mundo... por tercera vez');
+
+$("#id").val(solicitud.id);
+$("#nombre").val(solicitud.nombre);
+$("#apellido").val/(solicitud.apellido);
+
+$("li").on("click", function(event){
+
+    if( $("#detalle").is(':visible')) {
+        $("#detalle").hide();
+    } else {
+        $("#detalle").show();
+
+        let solicitud = event.target.val();
+
+        $("#id").val(solicitud.id);
+        $("#nombre").val(solicitud.nombre);
+        $("#apellido").val(solicitud.apellido);
+    }
+});
